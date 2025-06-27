34ef178dc4e34383590f2c30f3baf0914c262fcc
Naufal Ilham Ramadhan <111553406+nnnnaufalir@users.noreply.github.com>
2025-06-28 00:37:29 +0700

    Update 4. Tes lcd.ino NYOBA BANG

--- 4. Tes lcd.ino
+++ 4. Tes lcd.ino
@@ -2,7 +2,7 @@
 #include <U8g2lib.h>
 #include <U8x8lib.h>
 const char DEGREE_SYMBOL[] = { 0xB0, '\0' };
-U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=/ 10, / reset=*/ 8);
+U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=*/ 10, /* reset=*/ 8);
 
 void setup() {
 u8g2.begin();
@@ -24,13 +24,13 @@ void tulisanputih(){
 u8g2.setFont(u8g2_font_helvB10_tf);
 u8g2.drawBox(0,0,128,32); 
 u8g2.setColorIndex(0);
-u8g2.drawStr( 15, 20, "Aku nyoba wkwkwk");
+u8g2.drawStr( 46, 20, "NYOBA BANG");
 }
 
 void tulisan(){
 u8g2.setFont(u8g2_font_7x13B_tr );
 u8g2.setColorIndex(1);
-u8g2.drawStr( 30, 53, "00,00 KM/J");
+u8g2.drawStr( 38, 53, "00,00 KpJ");
 u8g2.drawFrame(0,32,128,32);    
 
 }
